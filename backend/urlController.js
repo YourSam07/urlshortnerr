4cf0c03939dd2993cf9770121ab5fc964a99027c
Sankalp Sethi <54805355+YourSam07@users.noreply.github.com>
2022-07-18 16:01:11 +0530

    "done"

---
+++ backend/urlController.js
@@ -0,0 +1,44 @@
+const Urls = require('./urlModel')
+const { nanoid }  = require('nanoid')
+const asyncHandler = require('express-async-handler')
+
+const generateURL = asyncHandler(async(req, res) =>  {
+  const urlExists = await Urls.findOne({originalUrl: req.body.url})
+
+  if (urlExists){
+    res.status(400)
+    return res.json({
+      message: 'Already Exists',
+    })
+  }
+  
+  const urlDetails = await Urls.create({
+    originalUrl: req.body.url,
+    shortenID: nanoid(7),
+  })
+
+  res.status(200).json({
+    ogUrl: req.body.url,
+    shortUrl: `${process.env.START_URL}${urlDetails.shortenID}`
+  });
+})
+
+const redirect = asyncHandler(async(req, res) => {
+  const code = req.params.id;
+
+  const sidExists = await Urls.findOne({shortenID: code})
+  console.log(sidExists);
+  if (!sidExists){
+    res.status(400)
+    return res.json({
+      message: "Url not shortened."
+    })
+  }
+
+  res.redirect(sidExists.originalUrl)
+})
+
+module.exports = {
+  generateURL,
+  redirect
+}
