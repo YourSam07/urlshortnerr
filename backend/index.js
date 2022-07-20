7b058d62fe3bafdc0d830b0f0753746521b29b12
NishantChaware <ndchaware99@gmail.com>
2022-07-20 13:30:37 +0530

    build

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,6 @@ app.post("/generateUrl", generateURL);
 app.get("/:id", redirect);
 
 
-const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
