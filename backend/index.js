f49d37e0ff1fa578ddbd4147795a67d6dc514302
NishantChaware <ndchaware99@gmail.com>
2022-07-20 12:58:52 +0530

    deploy changes

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/dbconnection')
-const dotenv = require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/dbconnection");
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
-const {generateURL, redirect} = require('./urlController');
+const { generateURL, redirect } = require("./urlController");
+const path = require("path");
 
-connectDB()
+connectDB();
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.post('/generateUrl', generateURL);
-app.get('/:id', redirect);
+app.post("/generateUrl", generateURL);
+app.get("/:id", redirect);
+app.get("/", (req, res) => {
+    res.send("Success")
+});
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Server started at port number: ${port}`));
-
