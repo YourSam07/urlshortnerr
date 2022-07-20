663def276711dfe78366350e6457d4f62e4805bf
NishantChaware <ndchaware99@gmail.com>
2022-07-20 13:46:32 +0530

    try 3

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,10 @@ app.post("/generateUrl", generateURL);
 app.get("/:id", redirect);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join("/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve("frontend", "build", "index.html"));
   });
 }
 
